6d778a56f8a3236ea27492cf425fd46bdea6b689
Đình Thi <cudinhthi120@gmail.com>
2022-12-26 18:35:21 +0700

    Update secure_storage.dart

--- lib/services/secure_storage.dart
+++ lib/services/secure_storage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class PinSecureStorage {
-  static const _storage = FlutterSecureStorage();
+const _storage = FlutterSecureStorage();
 
+class PinSecureStorage {
   static const _pinNumber = 'pin';
 
   // Set Pin number to secure storage
@@ -14,6 +14,37 @@ class PinSecureStorage {
       await _storage.read(key: _pinNumber);
 
   // Delete Pin number from secure storage
-  static deletePinNumber() async =>
-      await _storage.delete(key: _pinNumber);
+  static deletePinNumber() async => await _storage.delete(key: _pinNumber);
+}
+
+class QuestionSecureStorage {
+  static const _securityQuestion = 'question';
+
+  // Set security Quesion to secure storage
+  static Future setSecurityQuestion(String question) async =>
+      await _storage.write(key: _securityQuestion, value: question);
+
+  // Read security Question from secure storage
+  static Future<String?> getSecurityQuestion() async =>
+      await _storage.read(key: _securityQuestion);
+
+  // Delete security Question from secure storage
+  static deleteSecurityQuestion() async =>
+      await _storage.delete(key: _securityQuestion);
+}
+
+class AnswerSecureStorage {
+  static const _securityAnswer = 'answer';
+
+  // Set security Answer to secure storage
+  static Future setSecurityAnswer(String answer) async =>
+      await _storage.write(key: _securityAnswer, value: answer);
+
+  // Read security Answer from secure storage
+  static Future<String?> getSecurityAnswer() async =>
+      await _storage.read(key: _securityAnswer);
+
+  // Delete security Answer from secure storage
+  static deleteSecurityAnswer() async =>
+      await _storage.delete(key: _securityAnswer);
 }
