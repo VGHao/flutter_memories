89f0d25edb125577595c7ff8e03196b630a2f5c5
Đình Thi <cudinhthi120@gmail.com>
2023-01-16 22:41:56 +0700

    Update security_question.dart

--- lib/pages/security_question.dart
+++ lib/pages/security_question.dart
@@ -1,106 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter_memories_dailyjournal/pages/home_page.dart';
 import '../services/secure_storage.dart';
+import '../widgets/show_flush_bar.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class SecurityQuestion extends StatefulWidget {
-  static const route = 'question-page';
-
-  const SecurityQuestion({super.key});
+  final String checkEvent;
+  final String passcode;
+  const SecurityQuestion({
+    super.key,
+    required this.passcode,
+    required this.checkEvent,
+  });
 
   @override
   State<SecurityQuestion> createState() => _SecurityQuestionState();
 }
 
+TextEditingController _answer = TextEditingController();
+String checkUserEvent = '';
+String oldAnswer = '';
+String oldQuestion = '';
+String question = '';
+String answer = '';
+String confirmAnswer = '';
+String dropdownValue = list.first;
+
 class _SecurityQuestionState extends State<SecurityQuestion> {
   @override
   void initState() {
+    init();
     super.initState();
+  }
 
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      setQuestionDialog(context);
+  Future init() async {
+    String secureAnwser = await AnswerSecureStorage.getSecurityAnswer() ?? '';
+    String secureQuestion =
+        await QuestionSecureStorage.getSecurityQuestion() ?? '';
+
+    setState(() {
+      answer = "";
+      question = "";
+      oldAnswer = secureAnwser;
+      oldQuestion = secureQuestion;
+      checkUserEvent = widget.checkEvent;
+      print(checkUserEvent);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/passcode-img.png'),
-            fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: () async {
+        if (checkUserEvent == 'setQuestion') {
+          QuestionSecureStorage.deleteSecurityQuestion();
+          AnswerSecureStorage.deleteSecurityAnswer();
+          _answer.clear();
+          Navigator.pushReplacementNamed(context, 'set-lock');
+        } else {
+          _answer.clear();
+          Navigator.pushReplacementNamed(context, 'set-lock');
+        }
+        if (checkUserEvent == 'checkQuestionToLog') {
+          return false;
+        }
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/passcode-img.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-void setQuestionDialog(context) {
-  showGeneralDialog(
-    context: context,
-    pageBuilder: (BuildContext buildContext, Animation<double> animation,
-        Animation<double> secondaryAnimation) {
-      return Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.transparent,
-        child: DialogBody(),
-      );
-    },
-    barrierDismissible: true,
-    barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-    transitionDuration: const Duration(milliseconds: 200),
-  );
-}
-
-class DialogBody extends StatefulWidget {
-  const DialogBody({super.key});
-
-  @override
-  State<DialogBody> createState() => _DialogBodyState();
-}
-
-class _DialogBodyState extends State<DialogBody> {
-  // Read question and answer from secure storage
-  // String question = '';
-  // String answer = '';
-
-  // @override
-  // void initState() {
-  //   super.initState();
-
-  //   init();
-  // }
-
-  // Future init() async {
-  //   String userQuestion =
-  //       await QuestionSecureStorage.getSecurityQuestion() ?? '';
-  //   String userAnswer = await AnswerSecureStorage.getSecurityAnswer() ?? '';
-  //   setState(() {
-  //     question = userQuestion;
-  //     answer = userAnswer;
-  //   });
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Card(
-        margin: EdgeInsets.zero,
-        color: Colors.transparent,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              buildBackButton(context),
-              buildTitle(
-                context,
-                'Please set a security quesion in case you forget your password',
-              ),
-              DropdownButtonExample(),
-              AnswerField(),
-              ConfirmButton(),
-            ],
+          child: SafeArea(
+            child: Column(
+              children: [
+                buildBackButton(context),
+                checkUserEvent != 'checkQuestionToLog'
+                    ? buildTitle(
+                        context,
+                        'Please set a security quesion in case you forget your password',
+                      )
+                    : buildTitle(
+                        context,
+                        '',
+                      ),
+                const DropdownButtonExample(),
+                const AnswerField(),
+                ConfirmButton(
+                  passcode: widget.passcode,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -108,40 +101,92 @@ class _DialogBodyState extends State<DialogBody> {
   }
 
   buildBackButton(context) {
-    return Container(
-      alignment: Alignment.topLeft,
-      margin: const EdgeInsets.only(top: 12),
-      padding: const EdgeInsets.all(8),
-      child: Row(
-        children: [
-          MaterialButton(
-            onPressed: () {
-              // Delete Pin from secure storage
-              PinSecureStorage.deletePinNumber();
-              Navigator.pop(context);
-              Navigator.pushReplacementNamed(context, 'setting-page');
-            },
-            height: 50,
-            minWidth: 50,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
+    return checkUserEvent == 'checkQuestionToLog'
+        ? Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.only(top: 12),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                MaterialButton(
+                  onPressed: () async {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ChangePage(),
+                      ),
+                    );
+                    _answer.clear();
+                  },
+                  height: 50,
+                  minWidth: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                  ),
+                ),
+                const Text(
+                  'Reset Passcode',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
             ),
-            child: const Icon(
-              Icons.close,
-              color: Colors.white,
+          )
+        : Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.only(top: 12),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                MaterialButton(
+                  onPressed: () {
+                    if (oldAnswer == "") {
+                      QuestionSecureStorage.deleteSecurityQuestion();
+                      AnswerSecureStorage.deleteSecurityAnswer();
+                      _answer.clear();
+                      Navigator.pushReplacementNamed(context, 'set-lock');
+                    } else {
+                      _answer.clear();
+                      Navigator.pushReplacementNamed(context, 'set-lock');
+                    }
+                  },
+                  height: 50,
+                  minWidth: 50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                ),
+                checkUserEvent == "setQuestion"
+                    ? const Text(
+                        'Set Diary Lock',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    : const Text(
+                        'Change Security Question',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+              ],
             ),
-          ),
-          const Text(
-            'Set Diary Lock',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 
   buildTitle(context, title) {
@@ -174,12 +219,9 @@ const List<String> list = <String>[
 ];
 
 class _DropdownButtonExampleState extends State<DropdownButtonExample> {
-  String dropdownValue = list.first;
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 12),
       margin: const EdgeInsets.symmetric(horizontal: 24),
       decoration: BoxDecoration(
         border: Border.all(
@@ -187,43 +229,44 @@ class _DropdownButtonExampleState extends State<DropdownButtonExample> {
         ),
         borderRadius: BorderRadius.circular(4),
       ),
-      child: DropdownButton<String>(
-        value: dropdownValue,
-        menuMaxHeight: 200,
-        itemHeight: kMinInteractiveDimension,
-        icon: Expanded(
-          child: Container(
-            padding: const EdgeInsets.only(right: 12),
-            alignment: Alignment.topRight,
-            child: const Icon(
-              Icons.arrow_drop_down,
-              color: Colors.white,
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2<String>(
+          dropdownDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          value: dropdownValue,
+          itemHeight: kMinInteractiveDimension,
+          icon: Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(right: 12),
+              alignment: Alignment.topRight,
+              child: const Icon(
+                Icons.arrow_drop_down,
+                color: Colors.white,
+              ),
             ),
           ),
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+          ),
+          underline: Container(
+            height: 1,
+            color: Colors.black,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
-        elevation: 0,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 15,
-        ),
-        dropdownColor: Colors.black,
-        underline: Container(
-          height: 1,
-          color: Colors.black,
-        ),
-        onChanged: (String? value) {
-          // This is called when the user selects an item.
-          setState(() {
-            dropdownValue = value!;
-            QuestionSecureStorage.setSecurityQuestion(value);
-          });
-        },
-        items: list.map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
       ),
     );
   }
@@ -242,9 +285,7 @@ class _AnswerFieldState extends State<AnswerField> {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
       child: TextField(
-        onChanged: (value) {
-          AnswerSecureStorage.setSecurityAnswer(value);
-        },
+        controller: _answer,
         style: const TextStyle(
           color: Colors.white,
           fontSize: 15,
@@ -268,9 +309,15 @@ class _AnswerFieldState extends State<AnswerField> {
   }
 }
 
-class ConfirmButton extends StatelessWidget {
-  const ConfirmButton({super.key});
+class ConfirmButton extends StatefulWidget {
+  final String passcode;
+  const ConfirmButton({super.key, required this.passcode});
 
+  @override
+  State<ConfirmButton> createState() => _ConfirmButtonState();
+}
+
+class _ConfirmButtonState extends State<ConfirmButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -282,8 +329,61 @@ class ConfirmButton extends StatelessWidget {
       ),
       child: MaterialButton(
         onPressed: () async {
-          Navigator.pop(context);
-          Navigator.pushReplacementNamed(context, 'setting-page');
+          if (checkUserEvent == 'checkQuestionToLog') {
+            if (_answer.text.isEmpty) {
+              showFlushBar(context, "Please input your answer");
+            } else {
+              setState(() {
+                answer = _answer.text;
+              });
+              if (answer != oldAnswer || dropdownValue != oldQuestion) {
+                _answer.clear();
+                showFlushBar(context, 'Wrong Question or Answer');
+              } else {
+                _answer.clear();
+
+                await PinSecureStorage.deletePinNumber();
+                // ignore: use_build_context_synchronously
+                Navigator.pushReplacementNamed(context, '/');
+              }
+            }
+          } else {
+            if (_answer.text.isEmpty) {
+              showFlushBar(context, "Please input your answer");
+            } else {
+              if (answer == '') {
+                setState(() {
+                  answer = _answer.text;
+                  question = dropdownValue;
+                });
+                showFlushBar(context, 'Enter you answer again');
+                _answer.clear();
+              } else {
+                setState(() {
+                  confirmAnswer = _answer.text;
+                });
+                if (answer != confirmAnswer || question != dropdownValue) {
+                  _answer.clear();
+
+                  showFlushBar(context, 'Does not match');
+                } else {
+                  _answer.clear();
+                  await QuestionSecureStorage.setSecurityQuestion(
+                      dropdownValue);
+                  await AnswerSecureStorage.setSecurityAnswer(answer);
+                  if (widget.passcode != '') {
+                    await PinSecureStorage.setPinNumber(widget.passcode);
+                    await CheckUserSession.setUserSession('logged');
+                  }
+
+                  print("$dropdownValue, $answer, ${widget.passcode}");
+
+                  // ignore: use_build_context_synchronously
+                  Navigator.pushReplacementNamed(context, 'setting-page');
+                }
+              }
+            }
+          }
         },
         child: Text(
           'Confirm'.toUpperCase(),
