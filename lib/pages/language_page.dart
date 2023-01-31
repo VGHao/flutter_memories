0b23f80daf38e60080f041a824c162911b5a5719
Đình Thi <cudinhthi120@gmail.com>
2023-01-31 10:41:03 +0700

    Update language_page.dart

--- lib/pages/language_page.dart
+++ lib/pages/language_page.dart
@@ -1,6 +1,8 @@
+import 'package:confirm_dialog/confirm_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_memories_dailyjournal/services/secure_storage.dart';
+import 'package:restart_app/restart_app.dart';
 
 class LanguagePage extends StatefulWidget {
   const LanguagePage({super.key});
@@ -10,49 +12,6 @@ class LanguagePage extends StatefulWidget {
 }
 
 class _LanguagePageState extends State<LanguagePage> {
-  String currentLanguage = "en_US";
-
-  List listLanguage = [
-    {
-      'id': 0,
-      'title': 'English',
-      'isSelected': true,
-      'language_name': 'en_US',
-      'language': 'en',
-      'country': 'US',
-    },
-    {
-      'id': 1,
-      'title': 'Việt Nam',
-      'isSelected': false,
-      'language_name': 'vi',
-      'language': 'vi',
-      'country': '',
-    },
-  ];
-  @override
-  void initState() {
-    super.initState();
-
-    init();
-  }
-
-  Future init() async {
-    String language = await SelectedLanguage.getLanguage() ?? '';
-
-    setState(() {
-      currentLanguage = language;
-    });
-
-    for (var element in listLanguage) {
-      if (element["language_name"] == language) {
-        element["isSelected"] = true;
-      } else {
-        element["isSelected"] = false;
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,56 +26,126 @@ class _LanguagePageState extends State<LanguagePage> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 12),
-          Column(
-            children: List.generate(
-              listLanguage.length,
-              (index) => Container(
-                margin: const EdgeInsets.only(left: 24, right: 24, bottom: 12),
-                child: CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.trailing,
-                  contentPadding: const EdgeInsets.only(left: 16, right: 12),
-                  dense: true,
-                  side: MaterialStateBorderSide.resolveWith(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.selected)) {
-                      return BorderSide.none;
-                    }
-                    return BorderSide.none;
-                  }),
-                  title: Text(
-                    listLanguage[index]["title"],
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  value: listLanguage[index]["isSelected"],
-                  onChanged: (value) {
-                    if (listLanguage[index]["isSelected"] == true) {
-                      setState(() {
-                        listLanguage[index]["isSelected"] = true;
-                      });
-                    } else {
-                      setState(() {
-                        for (var element in listLanguage) {
-                          element["isSelected"] = false;
-                        }
-                        listLanguage[index]["isSelected"] = value;
-                        context.setLocale(Locale(
-                            listLanguage[index]["language"],
-                            listLanguage[index]["country"]));
-                        SelectedLanguage.setLanguage(context.locale.toString());
-                      });
-                    }
-                  },
+      body: const ListLanguage(),
+    );
+  }
+}
+
+class ListLanguage extends StatefulWidget {
+  const ListLanguage({super.key});
+
+  @override
+  State<ListLanguage> createState() => _ListLanguageState();
+}
+
+List listLanguage = [
+  {
+    'id': 0,
+    'title': 'English',
+    'isSelected': true,
+    'language_name': 'en_US',
+    'language': 'en',
+    'country': 'US',
+  },
+  {
+    'id': 1,
+    'title': 'Việt Nam',
+    'isSelected': false,
+    'language_name': 'vi',
+    'language': 'vi',
+    'country': '',
+  },
+];
+
+class _ListLanguageState extends State<ListLanguage> {
+  @override
+  void initState() {
+    super.initState();
+
+    init();
+  }
+
+  Future init() async {
+    String language = await SelectedLanguage.getLanguage() ?? '';
+
+    setState(() {
+      for (var element in listLanguage) {
+        if (element["language_name"] == language) {
+          element["isSelected"] = true;
+        } else {
+          element["isSelected"] = false;
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 12),
+        ...List.generate(
+          listLanguage.length,
+          (index) => Container(
+            margin: const EdgeInsets.only(left: 24, right: 24, bottom: 12),
+            child: CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.trailing,
+              contentPadding: const EdgeInsets.only(left: 16, right: 12),
+              dense: true,
+              side: MaterialStateBorderSide.resolveWith(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.selected)) {
+                  return BorderSide.none;
+                }
+                return BorderSide.none;
+              }),
+              title: Text(
+                listLanguage[index]["title"],
+                style: const TextStyle(
+                  fontSize: 16.0,
                 ),
               ),
+              value: listLanguage[index]["isSelected"],
+              onChanged: (value) {
+                if (listLanguage[index]["isSelected"] == true) {
+                  setState(() {
+                    listLanguage[index]["isSelected"] = true;
+                  });
+                } else {
+                  _confirmChangeDialog(context, value!, index);
+                }
+              },
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  Future<void> _confirmChangeDialog(
+      BuildContext context, bool value, int index) async {
+    if (await confirm(
+      context,
+      title: const Text('Change language'),
+      content: const Text("Restart app to save change?"),
+      textOK: const Text('OK'),
+      textCancel: const Text('CANCEL'),
+    )) {
+      return changeLanguage(value, index);
+    }
+    return;
+  }
+
+  void changeLanguage(bool value, int index) async {
+    setState(() {
+      for (var element in listLanguage) {
+        element["isSelected"] = false;
+      }
+      listLanguage[index]["isSelected"] = value;
+      context.setLocale(Locale(
+          listLanguage[index]["language"], listLanguage[index]["country"]));
+    });
+    await SelectedLanguage.setLanguage(context.locale.toString());
+    Restart.restartApp();
   }
 }
