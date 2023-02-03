d06299138f9738764a3523fd793553c7529716a5
Đình Thi <cudinhthi120@gmail.com>
2023-02-03 21:37:47 +0700

    Update backup_and_restore.dart

--- lib/pages/backup_and_restore.dart
+++ lib/pages/backup_and_restore.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class BackupAndRestore extends StatelessWidget {
+import '../services/secure_storage.dart';
+
+class BackupAndRestore extends StatefulWidget {
   const BackupAndRestore({super.key});
 
+  @override
+  State<BackupAndRestore> createState() => _BackupAndRestoreState();
+}
+
+class _BackupAndRestoreState extends State<BackupAndRestore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,73 +30,77 @@ class BackupAndRestore extends StatelessWidget {
   }
 }
 
-class BodyContent extends StatelessWidget {
+class BodyContent extends StatefulWidget {
   const BodyContent({super.key});
+
+  @override
+  State<BodyContent> createState() => _BodyContentState();
+}
+
+String userEmail = '';
+String userAvatar = '';
+
+class _BodyContentState extends State<BodyContent> {
+  GoogleSignIn googleSignIn = GoogleSignIn(
+    clientId:
+        "357589482333-sgggvbn10la0g2je2k862p93aonc0l4l.apps.googleusercontent.com",
+  );
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future init() async {
+    String getUserEmail = await CheckGoogleUserEmail.getUserEmail() ?? "";
+    String getUserAvatar = await CheckGoogleUserAvatar.getUserAvatar() ?? "";
+
+    setState(() {
+      userEmail = getUserEmail;
+      if (getUserAvatar != 'null') {
+        userAvatar = getUserAvatar;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const GoogleSignInTile(),
-        BackupAndRestoreTile(
-          title: 'Backup Data',
-          onTap: () {},
+        googleSigInTile(
+          // Tile onTap
+          userAvatar == ''
+              ? () {
+                  googleLogin();
+                }
+              : () {
+                  iconBtnBottomSheet();
+                },
+          // Icon Button OnPressed
+          () {
+            iconBtnBottomSheet();
+          },
         ),
-        BackupAndRestoreTile(
-          title: 'Data Recovery',
-          onTap: () {},
-        ),
+        userEmail != ''
+            ? Column(
+                children: [
+                  backupAndRestoreTitle(
+                    'Backup Data',
+                    () {},
+                  ),
+                  backupAndRestoreTitle(
+                    'Data Recovery',
+                    () {},
+                  )
+                ],
+              )
+            : const SizedBox(),
       ],
     );
   }
-}
 
-class GoogleSignInTile extends StatelessWidget {
-  const GoogleSignInTile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.only(left: 24, right: 24, top: 16),
-      child: ListTile(
-        onTap: () {},
-        leading: Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.blue,
-          ),
-          child: const Icon(
-            Icons.person,
-            color: Colors.white,
-          ),
-        ),
-        title: const Text('Backup Account'),
-        subtitle: const Text(
-          'Choose a Google Drive account',
-          style: TextStyle(
-            fontSize: 13,
-            color: Colors.grey,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class BackupAndRestoreTile extends StatelessWidget {
-  final String title;
-  final Function() onTap;
-  const BackupAndRestoreTile({
-    super.key,
-    required this.title,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
+  Widget backupAndRestoreTitle(String title, Function() onTap) {
     return Container(
       width: MediaQuery.of(context).size.width,
       margin: const EdgeInsets.only(left: 24, right: 24, top: 16),
@@ -97,5 +109,147 @@ class BackupAndRestoreTile extends StatelessWidget {
         title: Text(title),
       ),
     );
+  }
+
+  Widget googleSigInTile(Function() onTap, Function() onPressed) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.only(left: 24, right: 24, top: 16),
+      child: ListTile(
+          onTap: onTap,
+          trailing: userEmail != ""
+              ? IconButton(
+                  splashRadius: 20,
+                  icon: const Icon(Icons.more_vert_outlined),
+                  onPressed: onPressed,
+                )
+              : const SizedBox(),
+          leading: userAvatar != ""
+              ? CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                    userAvatar,
+                  ),
+                )
+              : Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.blue,
+                  ),
+                  child: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+          title: userEmail == ""
+              ? const Text('Choose a Google account')
+              : const Text('Backup Account'),
+          subtitle: userEmail == ""
+              ? googleSigInTileSubTitle('Choose a Google account')
+              : googleSigInTileSubTitle(userEmail)),
+    );
+  }
+
+  Widget googleSigInTileSubTitle(title) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontSize: 13,
+        color: Colors.grey,
+      ),
+    );
+  }
+
+  iconBtnBottomSheet() {
+    return showBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return bottomSheetBody();
+      },
+    );
+  }
+
+  Widget bottomSheetBody() {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Container(
+      padding: const EdgeInsets.only(top: 8),
+      height: height * 0.2,
+      width: width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(userEmail),
+          SizedBox(
+            width: width * 0.9,
+            height: 45,
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              child: const Text('Change account'),
+              onPressed: () {
+                googleChangeAccout();
+              },
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+            child: const Text(
+              'Remove account',
+              style: TextStyle(),
+            ),
+            onPressed: () {
+              googleSignOut();
+              setState(() {
+                userEmail = "";
+                userAvatar = "";
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  googleChangeAccout() async {
+    googleLogin();
+  }
+
+  googleLogin() async {
+    try {
+      var user = await GoogleSignIn().signIn();
+      await CheckGoogleUserAvatar.setUserAvatar((user!.photoUrl).toString());
+      await CheckGoogleUserEmail.setUserEmail(user.email);
+
+      setState(() {
+        userEmail = user.email;
+        if (user.photoUrl == null) {
+          userAvatar = "";
+        } else {
+          userAvatar = user.photoUrl!;
+        }
+      });
+    } catch (error) {
+      return null;
+    }
+  }
+
+  googleSignOut() async {
+    googleSignIn.signOut();
+    CheckGoogleUserEmail.deleteUserEmail();
+    CheckGoogleUserAvatar.deleteUserAvatar();
   }
 }
