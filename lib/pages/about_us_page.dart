96bd81cd697e8d730e70079b392ad8f6b42cf85a
Đình Thi <cudinhthi120@gmail.com>
2023-02-04 22:30:45 +0700

    Create about_us_page.dart

---
+++ lib/pages/about_us_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+
+class AboutUsPage extends StatefulWidget {
+  const AboutUsPage({super.key});
+
+  @override
+  State<AboutUsPage> createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: const Text('About Us'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/icon_launcher/icon_launcher.png',
+              scale: 2,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            html,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget html = Html(
+    data: """<p>
+                Memories - Daily Journal is a journaling application that helps users have a private place
+                to write down their thoughts, feelings, views on everything from work, study, love, friend …
+            </p>""",
+  );
+}
